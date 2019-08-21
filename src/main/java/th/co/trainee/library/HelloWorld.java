f7a0a4f89c919e4f2eeeff52bfdc0fa0c41c4aa1
Zchwazer <yamiyomuyumi@gmail.com>
2019-08-21 14:45:12 +0700

    Get Start

---
+++ src/main/java/th/co/trainee/library/HelloWorld.java
@@ -0,0 +1,12 @@
+package th.co.trainee.library;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
